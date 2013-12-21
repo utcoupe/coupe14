9ba52056d384f4093b2cb3837c672ab59ec7ce23
Tytan <quentin.chateau@gmail.com>
2013-12-21 07:07:15 +0100

    PULLUP

--- arduino/asserv/CC/compaArduino.cpp
+++ arduino/asserv/CC/compaArduino.cpp
@@ -9,12 +9,12 @@ extern Control control;
 
 void initPins(){
 	//set mode des pins pour arduino
-	pinMode(PIN_ENC_LEFT_A,INPUT);
-	pinMode(PIN_ENC_LEFT_B,INPUT);
-	pinMode(PIN_ENC_LEFT_0,INPUT);
-	pinMode(PIN_ENC_RIGHT_0,INPUT);
-	pinMode(PIN_ENC_RIGHT_A,INPUT);
-	pinMode(PIN_ENC_RIGHT_B,INPUT);
+	pinMode(PIN_ENC_LEFT_A,INPUT_PULLUP);
+	pinMode(PIN_ENC_LEFT_B,INPUT_PULLUP);
+	pinMode(PIN_ENC_LEFT_0,INPUT_PULLUP);
+	pinMode(PIN_ENC_RIGHT_0,INPUT_PULLUP);
+	pinMode(PIN_ENC_RIGHT_A,INPUT_PULLUP);
+	pinMode(PIN_ENC_RIGHT_B,INPUT_PULLUP);
 
 	//Definition des interruptions arduino en fonction du type d'évaluation
 #if ENCODER_EVAL == 4
