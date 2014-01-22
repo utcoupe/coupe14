377a31d5bd2c60bd62faf9d0f6c637a66efbe23d
Tytan <quentin.chateau@gmail.com>
2014-01-22 18:56:38 +0100

    suppression des parties inutiles

--- arduino/asserv/CC/serial_decoder.h
+++ arduino/asserv/CC/serial_decoder.h
@@ -1,7 +1,7 @@
 /****************************************
  * Author : Quentin C			*
  * Mail : quentin.chateau@gmail.com	*
- * Date : 18/12/13			*
+ * Date : 22/01/13			*
  ****************************************/
 #ifndef SERIAL_DECODER_H
 #define SERIAL_DECODER_H
@@ -10,12 +10,9 @@
 
 enum etape {ID_step,data_step,wait_step};
 void executeCmd(char serial_data);
-//int decode(unsigned char *data_in, unsigned char *data_out, int data_counter); //7bits -> 8bits (on garde le même tableau)
 int decode(unsigned char *data_in, unsigned char *data_out, int data_counter); //Decodage des données et vérification de corruption
-unsigned char left_shift_array(unsigned char *data_in, unsigned char *data_out, int data_counter, int shift); //Decalage de tableau à gauche
 unsigned char right_shift_array(unsigned char *data_in, unsigned char *data_out, int data_counter, int shift); //Decalage de tableau à droite
 int encode(unsigned char *data_in, unsigned char *data_out, int data_counter); //8bits -> 7bits (deux tableaux)
-//int check(unsigned char *data, int data_counter); //Vérifie la validité des données 8 bits
 void executeOrdre(unsigned char *data, int data_counter, unsigned char id, bool doublon);
 void sendResponse(unsigned char *data, int data_counter, unsigned char id); //Envoit un tableau de char 8 bits en réponse standard
 void sendInvalid(); //renvoit le code de message invalide (dépend de la plateforme)
