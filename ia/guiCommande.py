fd49b73f737177cf91c5201c174987be327683c9
Noxy <cedric.bache@gmail.com>
2014-01-27 00:12:54 +0100

    comm file d'ordre

--- ia/guiCommande.py
+++ ia/guiCommande.py
@@ -14,47 +14,40 @@ def gui(com):
 		address = 2
 		arguments = []
 		order = str(raw_input("Entre le nom ou le numéro d'un ordre:\n"))
-		if order[0] == 'A':
-			address = 2
-		elif order[0] == 'O':
-			address = 1
+		if order:
+			if order[0] == 'A':
+				address = 2
+			elif order[0] == 'O':
+				address = 1
 
-		if order == 'k':# arret d'urgence
-			com.sendOrderAPI(2, 'A_CLEANG', *arguments)
-		if order == 'a':# arret d'urgence
-			arguments = [1000, 500]
-			com.sendOrderAPI(2, 'A_GOTO', *arguments)
-			arguments = [1000, -500]
-			com.sendOrderAPI(2, 'A_GOTO', *arguments)
-			arguments = [0, 0, 0]
-			com.sendOrderAPI(2, 'A_GOTOA', *arguments)
-		if order == 's':
-			com.sendOrderAPI(1, 'O_BRAS_FERMER', *arguments)
-			arguments = [1000, 0]
-			com.sendOrderAPI(2, 'A_GOTO', *arguments)
-			arguments = [1400, 600]
-			com.sendOrderAPI(2, 'A_GOTO', *arguments)
-			arguments = [1400, 2000]
-			com.sendOrderAPI(2, 'A_GOTO', *arguments)
-			arguments = [1000, 2300]
-			com.sendOrderAPI(2, 'A_GOTO', *arguments)
-			arguments = [300, 2300]
-			com.sendOrderAPI(2, 'A_GOTO', *arguments)
-			arguments = [0, 0, 0]
-			com.sendOrderAPI(2, 'A_GOTOA', *arguments)
-		elif order in com.orders:
-			if isinstance(order, (int)):
-				order = com.orders[order]
+			if order == 'k':# arret d'urgence
+				com.sendOrderAPI(2, 'A_CLEANG', *arguments)
+			elif order == 'a':# arret d'urgence
+				arguments = [1000, 500]
+				com.sendOrderAPI(2, 'A_GOTO', *arguments)
+				arguments = [1000, -500]
+				com.sendOrderAPI(2, 'A_GOTO', *arguments)
+				arguments = [0, 0, 0]
+				com.sendOrderAPI(2, 'A_GOTOA', *arguments)
+			elif order == 's':
 
-			for typeToGet in ordersArguments[order]:
-				if typeToGet == 'int':
-					arguments.append(int(raw_input("Entre  un int\n")))
-				elif typeToGet == 'float':
-					arguments.append(float(raw_input("Entre un float\n")))
-				elif typeToGet == 'long':
-					arguments.append(long(raw_input("Entre  un long\n")))
-				else:
-					print "ERREUR: Parseur: le parseur a trouvé un type non supporté"
-			com.sendOrderAPI(address, order, *arguments)	
-		else:
-			print "L'ordre n'a pas été trouvé dans les fichiers arduino"
+				for a in range(300):
+					arguments = [1000, 0]
+					com.sendOrderAPI(2, 'A_GOTO', *arguments)
+
+			elif order in com.orders:
+				if isinstance(order, (int)):
+					order = com.orders[order]
+
+				for typeToGet in ordersArguments[order]:
+					if typeToGet == 'int':
+						arguments.append(int(raw_input("Entre  un int\n")))
+					elif typeToGet == 'float':
+						arguments.append(float(raw_input("Entre un float\n")))
+					elif typeToGet == 'long':
+						arguments.append(long(raw_input("Entre  un long\n")))
+					else:
+						print "ERREUR: Parseur: le parseur a trouvé un type non supporté"
+				com.sendOrderAPI(address, order, *arguments)	
+			else:
+				print "L'ordre n'a pas été trouvé dans les fichiers arduino"
