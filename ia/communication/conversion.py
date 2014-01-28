cc31599fc5b0b84a3e2e9b9ec1f7f19e995f9879
Noxy <cedric.bache@gmail.com>
2014-01-28 07:50:50 +0100

    optimisation

--- ia/communication/conversion.py
+++ ia/communication/conversion.py
@@ -11,8 +11,7 @@ def binaryToFloat(string):
 	    """
 	    See: http://en.wikipedia.org/wiki/IEEE_754-2008
 	    """
-	    from struct import pack,unpack
-	    return unpack("f",pack("I", bits2int(s)))
+	    return struct.unpack("f",struct.pack("I", bits2int(s)))
 
 	# Where the bits2int function converts bits to an integer.  
 	def bits2int(bits):
