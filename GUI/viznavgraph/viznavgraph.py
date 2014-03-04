0b1bba2413ecab8ff2af1b682398566afd5dac99
Tytan <quentin.chateau@gmail.com>
2014-03-04 16:29:20 +0100

    importation du navgrap 2012

---
+++ GUI/viznavgraph/viznavgraph.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+if __name__ == "__main__":
+	import sys
+	import os
+	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+	sys.path.append(os.path.join(FILE_DIR,"..","ia"))
+	sys.path.append(os.path.join(FILE_DIR,"..","libs"))
+	
+	import time
+	
+	from graphview import *
+	from navigation.nav import NavGraph
+	from geometry import Poly
+	
+	filename = os.path.join(FILE_DIR,"..","ia","map.xml")
+	offset= sys.argv[1]
+	start = time.time()
+	ng = NavGraph(offset, filename)
+	print("init time : %s" % (time.time() - start))
+	
+
+	dynamic_obstacle = Poly().initFromCircle((1500,1500),200,8)
+	ng.add_dynamic_obstacle(dynamic_obstacle)
+	ng.update()
+	
+	v = GraphView(ng,dynamic_obstacle)
+	v.mainloop()
+
