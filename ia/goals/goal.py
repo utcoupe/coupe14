050b32e3aecf38858a04e5188801da771bba7a01
Antoine Giraudmaillet <antoine-g@live.fr>
2014-03-04 13:14:28 +0100

    Add xml import & export support for goals

--- ia/goals/goal.py
+++ ia/goals/goal.py
@@ -1,15 +1,17 @@
 class Goal():
-	def __init__(self, name='Abstract', location=[0,0], points=0, priority=0, actions=[]):
-		self.__name		= name
-		self.__location = location
-		#TODO orientation ?
-		self.__points	= points
-		self.__priority	= priority # Bigger is higher
-		self.__actions	= actions
-		self.__distance	= Infinity
-		self.finished	= 0 # From 0 (not finished) to 100 (finished)
-		self.score		= 0
-		self.routing	= -1
+	FINISHED_THRESHOLD = 50
+
+	def __init__(self, name='Abstract', location=[0,0], orientation=0, points=0, priority=0, actions=[]):
+		self.__name			= name
+		self.__location 	= location
+		self.__orientation	= orientation # 0-360° (0 is x axis, 90 is y axis)
+		self.__points		= points
+		self.__priority		= priority # Bigger is higher
+		self.__actions		= actions
+		self.__distance		= Infinity
+		self.__finished		= 0 # From 0 (not finished) to 100 (finished)
+		self.score			= 0
+		self.routing		= -1
 
 	def __updateRouting(self, current_pos):
 		pass #TODO call navigation
@@ -27,10 +29,23 @@ class Goal():
 		return self.score
 
 	def incrementFinished(self, by_value):
-		self.finished += int(by_value)
+		self.__finished += int(by_value)
 
 	def __cmp__(self, other):
 		return self.score - other.score
 
 	def getName(self):
 		return self.name
+
+	def isFinished(self):
+		True if (finished > FINISHED_THRESHOLD) else False
+
+	def toXml(self):
+		__string =  "<goal name='" + __name + "'>\n"
+		__string += "\t<points>" + __points + "</points>\n\t<location-x>" + __location[0] + "<location-x>\n"
+		__string += "\t<location-x>" + __location[0] +"<location-y>\n\t<orientation>" + __orientation + "<orientation>\n"
+		__string += "\t<priority>" + __priority + "</priority>\n\t<finished>" + 0 + "</finished>\n\t<actions>"
+		for __action in __actions:
+			__string += "\n\t\t<action>" + __action + "</action>"
+		__string += "\n\t</actions>\n</goal>\n"
+		return __string
--- ia/goals/goalsManager.py
+++ ia/goals/goalsManager.py
@@ -1,15 +1,16 @@
 
 import heapq
+from xml.dom.minidom import parseString
 
 class GoalsManager():
-	FINISHED_LIMIT = 50
 
 	def __init__(self):
 		self.__available_goals = []
 		self.__finished_goals = []
+		loadGoals()
 
-		#TODO load goals from xml & fill __available_goals
-		heapq.heapify(__available_goals)
+	def __del__(self):
+		saveGoals()
 
 	def getBestGoal(self, current_pos):
 		for goal in __available_goals:
@@ -31,11 +32,48 @@ class GoalsManager():
 
 	def collectEnemyFinished(self, goal):
 		for goal in __available_goals:
-			if goal.finished > FINISHED_LIMIT:
+			if goal.isFinished
 				print 'Goal ' + goal.getName() + ' has been calculated accomplished by the enemy'
 				goal.score = -Infinity
 				__available_goals.sort()
 				__finished_goals.push(__available_goals.pop())
 
-	def saveState(filename="saved_goals.xml"):
-		pass #TODO
+	def loadGoals(filename='goals.xml'):
+		print 'GoalsManager: loading goals from: ' + filename
+		fd = open(filename,'r')
+		dom = parseString(fd.read())
+		fd.close()
+		for __xml_goal in dom.getElementsByTagName('goal'):
+			__name			= __xml_goal.attributes["name"].value
+			__x 			= __xml_goal.getElementsByTagName('location-x')[0].value
+			__y				= __xml_goal.getElementsByTagName('location-y')[0].value
+			__orientation	= __xml_goal.getElementsByTagName('orientation')[0].value
+			__points		= __xml_goal.getElementsByTagName('points')[0].value
+			__priority		= __xml_goal.getElementsByTagName('priority')[0].value
+			__finished		= (bool __xml_goal.getElementsByTagName('finished')[0].value)
+			__actions		= []
+
+			for __xml_action in __xml_goal.getElementsByTagName('action'):
+				__actions.append(__xml_action.value)
+
+			__goal = Goal(__name, [__x, __y], __orientation, __points, __priority, __actions)
+			__goal.incrementFinished(__finished)
+
+			if __goal.isFinished():
+				self.__available_goals.append(__goal)
+			else:
+				self.__finished_goals.append(__goal)
+
+		heapq.heapify(__available_goals)
+	
+	def saveGoals(filename='saved_goals.xml'):
+		print 'GoalsManager: saving goals to: ' + filename
+		__string = "<goals>\n"
+		for goal in self.__available_goals:
+			__string += goal.toXml()
+		__string += "</goals>"
+
+		doc = parseString(__string) #Check XML validity
+		with open(filename, "w") as f:
+    		f.write( doc.toxml() )
+    	f.close()
