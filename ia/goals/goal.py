226c5dee4ed422de1c6d7a563a0aed91af9b8a1c
Antoine Giraudmaillet <antoine-g@live.fr>
2014-03-04 18:41:22 +0100

    Refact code to use goal executions

--- ia/goals/goal.py
+++ ia/goals/goal.py
@@ -3,25 +3,12 @@
 class Goal:
 	FINISHED_THRESHOLD = 50
 
-	def __init__(self, name='Abstract', location=[0,0], orientation=0, points=0, priority=0, actions=[]):
+	def __init__(self, name='Abstract', type, location=[0,0], executions=[]):
 		self.__name			= name
+		self.__type			= type
 		self.__location 	= location
-		self.__orientation	= float(orientation) # 0-360° (0 is x axis, 90 is y axis)
-		self.__points		= int(points)
-		self.__priority		= int(priority) # Bigger is higher
-		self.__actions		= actions
-		self.__distance		= float("inf")
+		self.executions 	= executions
 		self.__finished		= 0 # From 0 (not finished) to 100 (finished)
-		self.score			= 0.0
-		self.routing		= -1
-
-	def __updateRouting(self, current_pos):
-		pass #TODO call navigation
-
-	def updateDistance(self, current_pos):
-		self.__updateRouting(current_pos)
-		#TODO calculate distance from routing, this should be delegated to navigationn
-		pass
 
 	def updateScore(self):
 		if self.isFinished():
@@ -33,9 +20,6 @@ class Goal:
 	def incrementFinished(self, by_value):
 		self.__finished += int(by_value)
 
-	def __cmp__(self, other):
-		return self.score - other.score
-
 	def getName(self):
 		return self.__name
 
@@ -46,11 +30,9 @@ class Goal:
 			False
 
 	def toXml(self):
-		string =  "<goal name='" + self.__name + "'>\n"
-		string += "\t<points>" + str(self.__points) + "</points>\n\t<location-x>" + str(self.__location[0]) + "</location-x>\n"
-		string += "\t<location-y>" + str(self.__location[0]) +"</location-y>\n\t<orientation>" + str(self.__orientation) + "</orientation>\n"
-		string += "\t<priority>" + str(self.__priority) + "</priority>\n\t<finished>" + str(self.__finished) + "</finished>\n\t<actions>"
-		for action in self.__actions:
-			string += "\n\t\t<action>" + action + "</action>"
-		string += "\n\t</actions>\n</goal>\n"
+		string =  "<goal name='" + self.__name + "'>\n\t<type>" + self.__type + "</type>\n"
+		string += "\t<finished>" + str(self.__finished) + "</finished>\n\t<executions>"
+		for execution in self.__executions:
+			string += "\n\t\t<execution>" + execution + "</execution>"
+		string += "\n\t</executions>\n</goal>\n"
 		return string
--- ia/goals/goalsManager.py
+++ ia/goals/goalsManager.py
@@ -6,8 +6,11 @@ from .goal import Goal
 class GoalsManager:
 
 	def __init__(self):
-		self.__available_goals = []
-		self.__finished_goals = []
+		self.__available_goals = [] #List of available goals
+		self.__finished_goals = [] #List of finished goals
+		self.__current_executions = [] # List of next chosen executions
+		self.__blocked_goals # List of blocked goals
+		self.__goal_types = dic()
 		self.loadGoals()
 		self.collectEnemyFinished()
 
@@ -17,33 +20,46 @@ class GoalsManager:
 	def __del__(self):
 		self.saveGoals()
 
-	def getBestGoal(self, current_pos):
+	def getBestGoalExecution(self, current_pos):
+		execution_heap = []
 		for goal in self.__available_goals:
-			goal.updateDistance(current_pos)
-			goal.updateScore()
-		self.__available_goals.sort()
-		print "GoalsManager:getBestGoal has chosen '%s'" % self.__available_goals[0].getName()
-		return self.__available_goals[0]
+			for goal_execution in goal.executions
+				goal_execution.updateDistance(current_pos)
+				goal_execution.updateScore()
+				execution_heap.append(goal_execution)
+				heapq.heapify(execution_heap)
+		execution = execution_heap[0]
+		associated_goal = execution.getGoal
+		self.__bolckGoal(associated_goal)
+		print "GoalsManager:getBestGoal has chosen '%s'" % associated_goal.getName()
+		return execution
 
-	def setFinished(self, goal_name):
-		for goal in self.__available_goals:
-			if goal.getName == goal_name:
-				goal.score = -float("inf")
-				__available_goals.sort()
-				__finished_goals.push(__available_goals.pop())
-				print 'Goal ' + goal.getName() + ' has been maked finished'
-				break
-			else:
-				raise 'Goal ' + goal.getName() + ' was not found when trying to be removed from heap'
+	def cancelExecution(self, execution):
+		self.__releaseGoal(execution.getGoal)
+
+	def finishExecution(self, execution):
+		self.__finishGoal(execution.getGoal)
+	
+	def __blockGoal(self, goal):
+		self.__blocked_goals.append(goal)
+		self.__available_goals.remove(goal)
+		print 'Goal ' + goal.getName() + ' was maked blocked'
+
+	def __releaseGoal(self, goal):
+		self.__available_goals.append(goal)
+		self.__blocked_goals.remove(goal)
+		print 'Goal ' + goal.getName() + ' was maked released'
+
+	def __finishGoal(self, goal):
+		self.__finished_goals.append(goal)
+		self.__blocked_goals.remove(goal)
+		print 'Goal ' + goal.getName() + ' was maked finished'
 
 	def collectEnemyFinished(self):
 		for goal in self.__available_goals:
-			print goal.isFinished()
 			if goal.isFinished():
 				print 'Goal ' + goal.getName() + ' has been calculated accomplished by the enemy'
-				goal.score = -float("inf")
-				self.__available_goals.sort()
-				self.__finished_goals.push(self.__available_goals.pop())
+				self.__finishGoal(goal)
 
 	# XML import and export of goals
 	def loadGoals(self, filename="goals/goals.xml"):
@@ -53,33 +69,37 @@ class GoalsManager:
 		fd.close()
 		for xml_goal in dom.getElementsByTagName('goal'):
 			name		= xml_goal.attributes["name"].value
-			x 			= xml_goal.getElementsByTagName('location-x')[0].firstChild.nodeValue
-			y			= xml_goal.getElementsByTagName('location-y')[0].firstChild.nodeValue
-			orientation	= xml_goal.getElementsByTagName('orientation')[0].firstChild.nodeValue
-			points		= xml_goal.getElementsByTagName('points')[0].firstChild.nodeValue
-			priority	= xml_goal.getElementsByTagName('priority')[0].firstChild.nodeValue
+			type		= xml_goal.getElementsByTagName('type')[0].firstChild.nodeValue
 			finished	= xml_goal.getElementsByTagName('finished')[0].firstChild.nodeValue
-			actions		= []
+			gx 			= xml_goal.getElementsByTagName('location-x')[0].firstChild.nodeValue
+			gy 			= xml_goal.getElementsByTagName('location-x')[0].firstChild.nodeValue
 
-			for xml_action in xml_goal.getElementsByTagName('action'):
-				actions.append(xml_action.firstChild.nodeValue)
-
-			goal = Goal(name, [x, y], orientation, points, priority, actions)
+			goal = Goal(name, type, [gx, gy])
 			goal.incrementFinished(finished)
+
+			for xml_execution in xml_goal.getElementsByTagName('goal'):
+				x 			= xml_execution.getElementsByTagName('location-x')[0].firstChild.nodeValue
+				y			= xml_execution.getElementsByTagName('location-y')[0].firstChild.nodeValue
+				orientation	= xml_execution.getElementsByTagName('orientation')[0].firstChild.nodeValue
+				points		= xml_execution.getElementsByTagName('points')[0].firstChild.nodeValue
+				priority	= xml_execution.getElementsByTagName('priority')[0].firstChild.nodeValue
+				time		= xml_execution.getElementsByTagName('time')[0].firstChild.nodeValue
+				actions		= []
+
+				for action	in xml_execution.getElementsByTagName('action'):
+					actions.append(action)
+				execution = GoalExecution(goal, [x, y], orientation, points, priority, actions, time)
+				goal.executions.append(execution)
 
 			if goal.isFinished():
 				self.__finished_goals.append(goal)
 			else:
 				self.__available_goals.append(goal)
-
-		heapq.heapify(self.__available_goals)
-		print "Available goals: %s" % self.__available_goals
-		print "Finished goals: %s" % self.__finished_goals
 	
 	def saveGoals(self, filename='goals/saved_goals.xml'):
 		print 'GoalsManager: saving goals to: ' + filename
 		string = "<goals>\n"
-		for heap in [self.__available_goals, self.__finished_goals]:
+		for heap in [self.__available_goals, self.__blocked_goals, self.__finished_goals]:
 			for goal in heap:
 				print goal.toXml()
 				string += goal.toXml()
