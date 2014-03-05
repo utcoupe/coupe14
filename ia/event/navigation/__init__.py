95045dd82e43fb3cb9677cfef289b07ae5b6e2e2
Tytan <quentin.chateau@gmail.com>
2014-03-05 17:36:55 +0100

    update, debug

--- ia/event/navigation/__init__.py
+++ ia/event/navigation/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
 FILE_DIR  = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(FILE_DIR,"..","..","libs"))
+sys.path.append(os.path.join(FILE_DIR,"..","..","..","libs"))
 
-import .pathfinding
+from .pathfinding import *
