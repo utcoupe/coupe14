731f8fb13685bef6ee3f9be28045c8bc1687804f
Tytan <quentin.chateau@gmail.com>
2014-03-08 20:26:57 +0100

    logging

--- ia/event/navigation/pathfinding.py
+++ ia/event/navigation/pathfinding.py
@@ -2,9 +2,12 @@
 from .pathfinding2012 import *
 from constantes import *
 from geometry import *
+import logging
+import time
 
 class PathFinding:
 	def __init__(self, robot_list, xml_filename, MARGE_PASSAGE=MARGE_PASSAGE):
+		self.__log = logging.getLogger(__name__)
 		flussmittel, tibot, big_enemy, small_enemy = robot_list
 		self.__flussmittel = flussmittel
 		self.__tibot = tibot
@@ -19,11 +22,13 @@ class PathFinding:
 			self.__our_bot_count += 1
 		if tibot != None:
 			self.__our_bot_count += 1
+		self.__log.info(str(self.__our_bot_count) + " robots amis actifs")
 		self.__number_of_enemy = 0
 		if big_enemy != None:
 			self.__number_of_enemy += 1
 		if small_enemy != None:
 			self.__number_of_enemy += 1
+		self.__log.info(str(self.__number_of_enemy) + " robots ennemis actifs")
 		#on met a jour tout les robots
 
 		if flussmittel != None:
@@ -33,18 +38,23 @@ class PathFinding:
 			self.__our_bot = self.__tibot
 			self.__other_bot = self.__flussmittel
 		else:
+			self.__log.error("Aucun robot ami actif !")
 			raise Exception("Aucun robot actif")
 		self.__other_bot_poly = Poly().initFromCircle(self.__other_bot.getPosition(), self.__other_bot.getRayon(), POINTS_PAR_CERCLE)
 		self.update(self.__our_bot)
 
 	def update(self, robot):
+		start_time = time.time()
 		self.__update_enemy_bot()
 		if self.__our_bot_count == 2: #Si on a deux robots, il faut compte le deuxieme dans le pathfinding
 			self.__update_our_bot(robot)
 		self.__ng.update()
+		self.__log.info("Mise à jour des polygones convexes en " + str((time.time() - start_time) * 1000) + "ms")
 
 	def getPath(self, start, end):
-		foo, bar, path = self.__ng.get_path(start,end)
+		start_time = time.time()
+		foo, bar, path = self.__ng.get_path(start, end)
+		self.__log.info("Calcul de trajectoire en " + str((time.time() - start_time) * 1000) + "ms : " + str(path))
 		return path
 
 	def getPolygons(self):
