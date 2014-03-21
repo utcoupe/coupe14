814f0e8e7b060b7d2e31c8d7558178c79f2b504c
Tytan <quentin.chateau@gmail.com>
2014-03-21 13:08:46 +0100

    premier objet sterovision

---
+++ supervisio/src/stereo/stereo.h
@@ -0,0 +1,31 @@
+#ifndef STEREO_H
+#define STEREO_H
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/types_c.h>
+
+using namespace cv;
+
+class Stereo {
+	public:
+	Stereo(int index_left=0, int index_right=1);
+	bool calibrate(int nbr_of_views=8, Size size_chessboard=Size(7,7));
+	void displayCalibration();
+	bool loadCalibration();
+	bool saveCalibration();
+
+	//SETTER
+	void setAlphaROI(double a);
+	private:
+	VideoCapture cam_left, cam_right;
+	bool calibrated;
+	Size cameras_image_size;
+	Mat Q; //disparity to depth matrix
+	Mat left_remap_1, left_remap_2, right_remap_1, right_remap_2; //Matrices de rectification de l'image
+	Rect ROI_left, ROI_right;
+
+	//parametre alpha de la fonction de detection des ROI
+	double alpha_parameter_roi;
+};
+
+#endif
