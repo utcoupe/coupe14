84e28610b890be307541fea9d20f4f219e002d91
Tytan <quentin.chateau@gmail.com>
2014-04-10 17:27:40 +0200

    timings precis

--- supervisio/src/traitement/timings.cpp
+++ supervisio/src/traitement/timings.cpp
@@ -1,24 +1,31 @@
 #include <ctime>
+#include <sys/time.h>
 #include <iostream>
 #include "timings.h"
 #include "../global.h"
 
 using namespace std;
 
+long timeMicros() {
+        struct timeval tv;       
+        if(gettimeofday(&tv, NULL) != 0) return 0;
+        return (unsigned long)((tv.tv_sec * 1000000ul) + (tv.tv_usec));        
+}
+
 void timings(string name) {
 	if (TIMEBENCH) {
 		static bool init = true;
-		static time_t start, end;
+		static long start, end;
 		if (name == "") {
-			init == true;
+			init = true;
 		}
 		if (init) {
-			time(&start);
+			start = timeMicros();
 			init = false;
 		}
 		else {
-			time(&end);
-			cout << name << difftime(end, start) << endl;
+			end = timeMicros();
+			cout << name << (end - start)/1000.0 << "ms" << endl;
 			start = end;
 		}
 	}
