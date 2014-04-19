5fc00aa8dca2a063a7055c9742b70011008e2459
Tytan <quentin.chateau@gmail.com>
2014-04-19 13:07:56 +0200

    en cours refonte timings

--- supervisio/src/traitement/timings.cpp
+++ supervisio/src/traitement/timings.cpp
@@ -12,22 +12,62 @@ long timeMicros() {
         return (unsigned long)((tv.tv_sec * 1000000ul) + (tv.tv_usec));        
 }
 
-void timings(string name) {
-	if (TIMEBENCH) {
-		static bool init = true;
-		static long start, end;
-		if (name == "") {
-			init = true;
+Timings::Timings() {
+	step = start = timeMicros();
+}
+
+float Timings::igetTime() {
+	step = timeMicros();
+	return (step - start)/1000.0;
+}
+
+float Timings::igetStepTime() {
+	long new_step = timeMicros();
+	long time = (new_step - step)/1000.0;
+	step = new_step;
+	return time;
+}
+
+void Timings::startTimer(int i) {
+	if (i = nb_instances) {
+		if (nb_max <= nb_instances) {
+			Timings** temp = instances;
+			instances = new Timings* [nb_instances + 5];
+			for (int j=0; j < nb_instances; j++) {
+				instances[j] = temp[j];
+			}
+			nb_max = nb_instances + 5;
+			delete[] temp;
 		}
-		if (init) {
-			start = timeMicros();
-			init = false;
-		}
-		else {
-			end = timeMicros();
-			cout << name << (end - start)/1000.0 << "ms" << endl;
-			start = end;
-		}
+		instances[nb_instances] = new Timings();
+	}
+	if (i < nb_instances) {
+		delete instances[i];
+		instances[i] = new Timings();
+	}
+	else {
+		cerr << "Timins index incorrect" << endl;
 	}
 }
 
+float Timings::getTime(int i) {
+	if (i < nb_instances) {
+		return instances[i]->igetTime();
+	}
+	else return 0;
+}
+
+float Timings::getStepTime(int i) {
+	if (i < nb_instances) {
+		return instances[i]->igetStepTime();
+	}
+	else return 0;
+}
+
+void Timings::writeTime(int i, string str) {
+	cout << str << " : " << getTime(i) << endl;
+}
+
+void Timings::writeStepTime(int i, string str) {
+	cout << str << " : " << getStepTime(i) << endl;
+}
--- supervisio/src/traitement/timings.h
+++ supervisio/src/traitement/timings.h
@@ -3,6 +3,21 @@
 
 #include <iostream>
 
+class Timings {
+	public:
+		static void startTimer(int i);
+		static float getTime(int i);
+		static float getStepTime(int i);
+		static void writeTime(int i, std::string str="");
+		static void writeStepTime(int i, std::string str="");
+	private:
+		static Timings** instances;
+		static int nb_instances = 0, nb_max = 0;
+		Timings();
+		float igetTime();
+		float igetStepTime();
+		long start, step;
+}
 void timings(std::string name = "");
 
 #endif
