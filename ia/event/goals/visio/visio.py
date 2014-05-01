21ceb185657946d9336a6d54fd5bbe5074f14de8
Tytan <quentin.chateau@gmail.com>
2014-05-01 17:34:27 +0200

    python visio avec path config

--- ia/event/goals/visio/visio.py
+++ ia/event/goals/visio/visio.py
@@ -3,6 +3,7 @@ import logging
 from subprocess import Popen, PIPE
 import time
 import atexit
+import sys
 
 
 """TODO : Post-traitement des positions des triangles pour corriger
@@ -35,7 +36,7 @@ class Triangle:
 
 
 class Visio:
-	def __init__(self, path_exec, index=0, big_bot=None):
+	def __init__(self, path_exec, index=0, path_config='./', big_bot=None):
 		#Parameters
 		self.__updatePeriod = 0.001  # période d'attente entre deux demandes au client
 		self.__retry_count = 0
@@ -50,10 +51,9 @@ class Visio:
 
 		#Lancement du client
 		self.__log.info("Executing C++ program")
-		self.client = Popen([self.path_exec, str(index)], stdin=PIPE, universal_newlines=True, stdout=PIPE)
+		self.client = Popen([self.path_exec, str(index), 'com', path_config], stdin=PIPE, universal_newlines=True, stdout=PIPE)
 		atexit.register(self.client.kill)
 		stdout = ''
-		time.sleep(1)
 		while stdout != 'READY\n':
 			#Attente des données client
 			stdout = self.client.stdout.readline()
