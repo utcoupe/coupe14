c4877a16fabdddaf4c69499ce19bea0154d98e34
Tytan <quentin.chateau@gmail.com>
2014-05-11 00:07:39 +0200

    ouverture et push coords sur piep

--- hokuyo/src/communication.c
+++ hokuyo/src/communication.c
@@ -1,8 +1,25 @@
 #include "communication.h"
 #include "fast_math.h"
 
-void init_protocol() {
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdio.h>
+
+static FILE *pipe;
+
+void init_protocol(char *path) {
+	char *pipe_path = strcat(path, "/hokuyo_pipe");
+	mkfifo(pipe_path, 0666);
+	pipe = fopen(pipe_path, "w+");
 }
 
+// !t;x1:y1;x2:y2;\n
 void pushResults(struct coord *coords, int nbr, long timestamp) {
+	int i=0;
+	fprintf(pipe, "!%ld;", timestamp);
+	for(i=0; i<nbr; i++) {
+		fprintf(pipe, "%d:%d;", coords[i].x, coords[i].y);
+	}
+	fprintf(pipe, "\n");
 }
