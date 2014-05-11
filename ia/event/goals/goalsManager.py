f7a424b32d8d131248c857f5860e55bbfcdef0ce
Alexis Schad <schadoc_alex@hotmail.fr>
2014-05-11 17:15:41 +0200

    Résolution erreur merge

--- ia/event/goals/goalsManager.py
+++ ia/event/goals/goalsManager.py
@@ -169,7 +169,7 @@ class GoalsManager:
 				orders = prev_action
 
 			#on ajoute la trajectoire calculé
-			orders.extend(self.__tupleTrajectoireToDeque(tuple_trajectoire_list[1:]))
+			orders.extend(self.__goalsLib.tupleTrajectoireToDeque(tuple_trajectoire_list[1:]))
 			orders.append( ("A_ROT", (goal.getElemGoalOfId(elem_goal_id).getPositionAndAngle()[2],)) )
 			#on ajoute attend d'être arrivé pour lancer les actions
 			orders.append( ("THEN", ()) )
