0e533710d2d9b7ffd95f12c472b50c157218daea
Tytan <quentin.chateau@gmail.com>
2014-05-16 19:00:08 +0200

    plus de print pour plus de debug

--- raspi_exec/src/com.h
+++ raspi_exec/src/com.h
@@ -16,7 +16,7 @@ struct hokData {
 };
 
 void com_loop(const char* cam_pipe, const char* hok_pipe) ;
-void parseCamera(char *line);
-void parseHokuyo(char *line);
+void parseCamera(char *ori_line);
+void parseHokuyo(char *ori_line);
 
 #endif
