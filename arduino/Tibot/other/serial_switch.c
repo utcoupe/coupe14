f47a9637beea17fd6c6e71e89b53b7e71a8d8dd2
Tytan <quentin.chateau@gmail.com>
2014-05-17 18:39:10 +0200

    gestion doublons et pause Tibot

--- arduino/Tibot/other/serial_switch.c
+++ arduino/Tibot/other/serial_switch.c
@@ -12,6 +12,7 @@
 #include "serial_types.h"
 
 extern int jack_state;
+static bool paused = false;
 
 //La fonction renvoit le nombre d'octet dans ret, chaine de caractère de réponse. Si doublon, ne pas executer d'ordre mais renvoyer les données à renvoyer
 int switchOrdre(unsigned char ordre, unsigned char *argv, unsigned char *ret, bool doublon){ 
@@ -19,19 +20,23 @@ int switchOrdre(unsigned char ordre, unsigned char *argv, unsigned char *ret, bo
 	int ret_size = 0;
 	switch(ordre){
 	case O_TIR_FILET:
-		last_id = btoi(argv);
-		tirFilet();
-		break;
-	case O_BALAI: {
-		last_id = btoi(argv);
-		int side = btoi(argv+2);
-		balai(side);
+		if (!doublon) {
+			last_id = btoi(argv);
+			tirFilet();
 		}
 		break;
-	case O_TIR_BALLE: {
-		last_id = btoi(argv);
-		int tirs = btoi(argv+2);
-		tirBalles(tirs);
+	case O_BALAI:
+		if (!doublon && !paused) {
+			last_id = btoi(argv);
+			int side = btoi(argv+2);
+			balai(side);
+		}
+		break;
+	case O_TIR_BALLE:
+		if (!doublon && !paused) {
+			last_id = btoi(argv);
+			int tirs = btoi(argv+2);
+			tirBalles(tirs);
 		}
 		break;
 	case GET_LAST_ID:
@@ -44,6 +49,12 @@ int switchOrdre(unsigned char ordre, unsigned char *argv, unsigned char *ret, bo
 	case O_JACK_STATE:
 		ret_size = 2;
 		itob(jack_state, ret);
+		break;
+	case PAUSE:
+		paused = true;
+		break;
+	case RESUME:
+		paused = false;
 		break;
 	default:
 		return -1;//commande inconnue
