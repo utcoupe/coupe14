c62393cf135d3a2e3d00b482e11945941b066f69
Tytan <quentin.chateau@gmail.com>
2014-05-28 19:10:44 +0200

    tentative resolution bug tri not defined

--- ia/event/goals/visio/visio.py
+++ ia/event/goals/visio/visio.py
@@ -168,27 +168,25 @@ class Visio:
 			return
 
 		for i in range(len(self._triangles)):
-			tri = self._triangles[i]
-
 			#calcul des coordonnées relatives dans le repère des coords absolues
 			robot_angle = self.__big_bot["getPositionAndAngle"][2]
 			self.__log.info("Got robot in position : " + str(self.__big_bot["getPosition"]) + " with angle = " + str(self.__big_bot["getPositionAndAngle"][2]))
 
-			#calcul des coordonnées réelles du triangles, on le recalcule par la
+			#calcul des coordonnées réelles du self._triangles[i]angles, on le recalcule par la
 			# suite si elles sont a modifier, mais on en a besoin pour savoir
 			# s'i faut les modifier
-			tri.real_coords = [i + j for i, j in zip(tri.rel_in_abs(robot_angle), self.__big_bot["getPosition"])]
-			self.__log.info("realtive coords : " + str(tri.coords))
-			self.__log.info("rel in ab : " + str(tri.rel_in_abs(robot_angle)))
-			self.__log.info("real : " + str(tri.real_coords))
+			self._triangles[i].real_coords = [i + j for i, j in zip(tri.rel_in_abs(robot_angle), self.__big_bot["getPosition"])]
+			self.__log.info("realtive coords : " + str(self._triangles[i].coords))
+			self.__log.info("rel in ab : " + str(self._triangles[i].rel_in_abs(robot_angle)))
+			self.__log.info("real : " + str(self._triangles[i].real_coords))
 
-			#Traitement de la position pour modif si triangle en hauteur
-			if self.__outOfMap(tri) or self.__inFruitZone(tri) or self.__inStartZone(tri):
-				self.__log("Removed detected triangle at coords " + str(tri.real_coords))
-				self._triangles.remove(tri)
+			#Traitement de la position pour modif si self._triangles[i]angle en hauteur
+			if self.__outOfMap(self._triangles[i]) or self.__inFruitZone(tri) or self.__inStartZone(tri):
+				self.__log("Removed detected self._triangles[i]angle at coords " + str(tri.real_coords))
+				self._self._triangles[i]angles.remove(tri)
 
-			#elif self.__inHighGround(tri):
-			#	self.__highGroundProcess(tri, self.__hplat)
+			#elif self.__inHighGround(self._triangles[i]):
+			#	self.__highGroundProcess(self._triangles[i], self.__hplat)
 
 	def __highGroundProcess(self, tri, hobj):
 		"""Corrige les coordonnées des triangles en hauteurs à des positions connues
