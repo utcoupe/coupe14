883840e14f2ba13df371b33f5b0a4443f304a254
Tytan <quentin.chateau@gmail.com>
2014-05-29 01:18:03 +0200

    gestion pwm 0 stoppante tibot

--- arduino/Tibot/asserv/motor.cpp
+++ arduino/Tibot/asserv/motor.cpp
@@ -13,34 +13,36 @@ Motor motor_left(MOTOR_LEFT);
 Motor motor_right(MOTOR_RIGHT);
 
 void set_pwm_left(int pwm){
-	pwm = -pwm;//les moteurs sont faces à face, pour avancer il faut qu'il tournent dans un sens différent
-	if (pwm > 0)
-		pwm += PWM_MIN;
-	else if (pwm < 0)
-		pwm -= PWM_MIN;
+	if (pwm != NO_PWM) {
+		pwm = -pwm;//les moteurs sont faces à face, pour avancer il faut qu'il tournent dans un sens différent
+		if (pwm > 0)
+			pwm += PWM_MIN;
+		else if (pwm < 0)
+			pwm -= PWM_MIN;
 
-	pwm += 127;
+		pwm += 127;
 
-	if(pwm > 255)
-		pwm = 255;
-	else if(pwm < 0)
-		pwm = 0;
-	
+		if(pwm > 255)
+			pwm = 255;
+		else if(pwm < 0)
+			pwm = 0;
+	}
 	motor_left.setPwm(pwm);
 }
 
 void set_pwm_right(int pwm){
-	if (pwm > 0)
-		pwm += PWM_MIN;
-	else if (pwm < 0)
-		pwm -= PWM_MIN;
+	if (pwm != NO_PWM) {
+		if (pwm > 0)
+			pwm += PWM_MIN;
+		else if (pwm < 0)
+			pwm -= PWM_MIN;
 
-	pwm += 127;
+		pwm += 127;
 
-	if(pwm > 255)
-		pwm = 255;
-	else if(pwm < 0)
-		pwm = 0;
-	
+		if(pwm > 255)
+			pwm = 255;
+		else if(pwm < 0)
+			pwm = 0;
+	}
 	motor_right.setPwm(pwm);
 }
